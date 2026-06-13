d99c3b0ef413764e5a88b135eca16026b1646f1a
Teddyseetink <hovanhai2997@gmail.com>
2026-06-13 21:35:21 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,5 +3,5 @@ local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https
 if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 or placeId == 122478697296975 or placeId == 73902483975735 or UniverseID == 994732206 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/e86ed284a22672d29e906214e7bbf8b9.lua"))() 
 elseif UniverseID == 10200395747 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/...../refs/heads/main/GrowaGarden2.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/growagadern2"))()
 end
