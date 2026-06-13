18f54c1d9741880c5b85e1ccfbd83f81a55f84b3
Teddyseetink <hovanhai2997@gmail.com>
2026-06-13 21:38:39 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-local placeId = game.PlaceId
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 or placeId == 122478697296975 or placeId == 73902483975735 or UniverseID == 994732206 then
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/e86ed284a22672d29e906214e7bbf8b9.lua"))() 
-elseif UniverseID == 10200395747 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/growagadern2"))()
+local universeId = game.GameId 
+if universeId == 994732206 then
+    runScript("https://api.luarmor.net/files/v4/loaders/e86ed284a22672d29e906214e7bbf8b9.lua")
+
+elseif universeId == 10200395747 then
+    runScript("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/growagadern2.lua")
 end
